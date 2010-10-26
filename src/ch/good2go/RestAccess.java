5c48c2277c390ecdbc898143d02d494f17ed1d88
Roman <roman.simecek@screenconcept.ch>
2010-10-26 17:58:41 +0200

    Initial android app commit.

---
+++ src/ch/good2go/RestAccess.java
@@ -0,0 +1,14 @@
+package ch.good2go;
+
+import ch.good2go.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RestAccess extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
