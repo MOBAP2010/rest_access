364db45c4c27edb09487a96eb10848ceb96cd17d
Roman <roman.simecek@screenconcept.ch>
2010-11-08 15:57:44 +0100

    Button for testing and first REST get method implementation.

--- src/ch/good2go/RestAccess.java
+++ src/ch/good2go/RestAccess.java
@@ -1,14 +1,23 @@
 package ch.good2go;
 
-import ch.good2go.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RestAccess extends Activity {
+	
+	private static String testUrlRessource = "http://192.168.1.33:3000/devices/3.json";
+	private static String testUrlRessources = "http://192.168.1.33:3000/devices.json";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void getRessource(View view){
+    	RestClient.getRessources(testUrlRessources);
+    	//RestClient.getSingleRessource(testUrlRessource);
     }
 }
